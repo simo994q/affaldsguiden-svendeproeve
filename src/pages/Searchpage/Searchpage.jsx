3806a32e16854770b83eeb5f3a83f8dcc7405841
simo994q <simonkn1@hotmail.com>
2024-03-04 13:12:53 +0100

    start of search

--- src/pages/Searchpage/Searchpage.jsx
+++ src/pages/Searchpage/Searchpage.jsx
@@ -1,15 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { SortingCard } from '../../components/SortingCard/SortingCard'
 
 export const Searchpage = () => {
 
     const { query } = useParams()
 
+    const [results, setResults] = useState()
+
     useEffect(() => {
-        console.log(query);
+        fetch(`http://localhost:3000/search/${query}`)
+            .then(res => res.json())
+            .then(data => setResults(data))
     }, [])
 
+    console.log(results)
+
     return (
-        <div>Searchpage</div>
+        <>
+            {results ?
+                <>
+                    {
+                        results.num_result !== 0 ?
+                            <>
+                                <h1>Din søgning gav {results.num_result} resultater</h1>
+                                <div>
+                                    {results.data.map((result) => {
+                                        
+                                    })}
+                                </div>
+                            </>
+                            :
+                            <>
+                                <h1>Din søgning gav ingen resultater</h1>
+                            </>
+                    }
+                </>
+                :
+                <>Loading...</>
+            }
+        </>
     )
 }
