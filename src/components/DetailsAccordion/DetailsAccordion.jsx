4b9abf9c78e24802fe1570f5b6a2b15c8005334e
simo994q <simonkn1@hotmail.com>
2024-03-05 14:56:11 +0100

    sorting and sections details lowkey done

---
+++ src/components/DetailsAccordion/DetailsAccordion.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import style from './DetailsAccordion.module.scss'
+
+export const DetailsAccordion = ({ details }) => {
+    const { title, image_filepath, types } = details
+
+    let notAllowed = []
+    let Allowed = []
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    //allowed or not allowed
+    types.map((type) => {
+        if (type.rules.is_allowed === true) {
+            Allowed.push(type)
+        } else {
+            notAllowed.push(type)
+        }
+    })
+
+    const checkAllow = (rules) => {
+        const { is_station, is_home } = rules
+        if (is_station && is_home) {
+            return 'Hjemme og station'
+        }
+        else if (is_station && !is_home) {
+            return 'Station'
+        }
+        else if (!is_station && is_home) {
+            return 'Hjemme'
+        }
+    }
+
+    const checkColor = (rules) => {
+        const { is_station, is_home } = rules
+        if (is_station && is_home) {
+            return '#90EE90'
+        }
+        else if (is_station && !is_home) {
+            return '#FFBF00'
+        }
+        else if (!is_station && is_home) {
+            return '#6495ED'
+        }
+    }
+
+    return (
+        <>
+            <div className={style.wrapper}>
+                <div className={style.infoHeader}>
+                    <img src={image_filepath} alt="" />
+                    <h1>{title}</h1>
+                </div>
+                <div style={{display: isOpen ? 'flex' : 'none'}} className={style.fullInfoWrapper}>
+                    <div className={style.infoWrapper}>
+                        <h2>Hvad modtager vi?</h2>
+                        <hr />
+                        <div className={style.allowedSection}>
+                            {
+                                Allowed.map((type) => {
+                                    return (
+                                        <div key={type.id}>
+                                            <p className={style.title}>{type.title}</p>
+                                            <p className={style.titleInfo} style={{ backgroundColor: checkColor(type.rules) }}>{checkAllow(type.rules)}</p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                    <div className={style.infoWrapper}>
+                        <h2>Hvad modtager vi ikke?</h2>
+                        <hr />
+                        <div className={style.notAllowedSection}>
+                            {
+                                notAllowed.map((type) => {
+                                    return (
+                                        <div key={type.id}>
+                                            <p className={style.title}>{type.title}</p>
+                                            <p className={style.titleInfoNo}>Nej tak</p>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+                <img
+                    onClick={() => setIsOpen(!isOpen)}
+                    className={style.openArrow}
+                    style={{ rotate: isOpen ? '0deg' : '180deg' }}
+                    src="/src/assets/images/layout/icon-arrow-up.svg" alt="" />
+            </div>
+        </>
+    )
+}
--- src/pages/Sectionpage/Sectionpage.jsx
+++ src/pages/Sectionpage/Sectionpage.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import style from './Sectionpage.module.scss'
+import { DetailsAccordion } from '../../components/DetailsAccordion/DetailsAccordion'
 
 export const Sectionpage = () => {
 
     const { section_id } = useParams()
 
     const [sectionData, setSectionData] = useState()
-    const [sectionCategories, setSectionCategories] = useState()
+    const [sectionCategories, setSectionCategories] = useState([])
 
     useEffect(() => {
         fetch(`http://localhost:3000/section/${section_id}`)
@@ -17,12 +18,10 @@ export const Sectionpage = () => {
 
     useEffect(() => {
         if (sectionData) {
-            let empty = []
-            sectionData.categories.map((cat) => {
-                fetch(`http://localhost:3000/category/details/${cat.id}`)
+            Promise.all(sectionData.categories.map(async (category) => {
+                return fetch(`http://localhost:3000/category/details/${category.id}`)
                     .then(res => res.json())
-                    .then(data => { empty.push(data), setSectionCategories(empty) })
-            })
+            })).then(data => setSectionCategories(data))
         }
     }, [sectionData])
 
@@ -34,9 +33,20 @@ export const Sectionpage = () => {
             {sectionData ?
                 <>
                     <div className={style.wrapper}>
-                        <div style={{backgroundColor: `#${sectionData.color}`}} className={style.header}>
+                        <div style={{ backgroundColor: `#${sectionData.color}` }} className={style.header}>
                             <h1>{sectionData.title}</h1>
                             <img src={sectionData.filepath} alt="" />
+                        </div>
+                        <div className={style.detailsWrapper}>
+                            {sectionCategories ?
+                                <>
+                                    {sectionCategories.map((item) => {
+                                        return <DetailsAccordion  key={item.id} details={item} />
+                                    })}
+                                </>
+                                :
+                                <>Loading...</>
+                            }
                         </div>
                     </div>
                 </>
